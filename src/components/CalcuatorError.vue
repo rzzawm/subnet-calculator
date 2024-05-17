b237f4ddc5df7aadf4e4d3e88a9713deda083cab
Reza Eshraghi <rzzawm@gmail.com>
2024-05-17 14:04:56 +0330

    style error section

--- src/components/CalcuatorError.vue
+++ src/components/CalcuatorError.vue
@@ -3,7 +3,10 @@ import { IP } from '../store'
 </script>
 
 <template>
-    <div class="-order-1">
-        <p>{{ IP.errorMsg }}</p>
+    <div class="max-md:-order-1 grid place-items-center">
+        <div class="flex flex-col items-center gap-4">
+            <img src="/warning.png" width="128px" alt="error">
+            <p class="text-amber-900 dark:text-amber-600 text-sm italic font-semibold">{{ IP.errorMsg }}</p>
+        </div>
     </div>
 </template>
